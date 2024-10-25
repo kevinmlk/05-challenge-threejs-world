13007026001a63af632d83fc93643272a4d0266f
Kevin Malekera <r0900654@student.thomasmore.be>
2024-10-25 15:10:07 +0200

    feat: added antialias

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 // Create renderer
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  // Set antialias
+  antialias: true
+});
 
 // Set renderer size
 renderer.setSize( window.innerWidth, window.innerHeight );
